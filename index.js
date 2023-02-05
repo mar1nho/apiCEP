8e3c3a7fb965a5ba41deac3bff52e0b1ec42ac26
Marinho <120027666+mar1nho@users.noreply.github.com>
2023-02-05 13:22:36 -0300

    cpf not found error

--- index.js
+++ index.js
@@ -6,11 +6,23 @@ async function getAddress(postalCode) {
     let cep = document.getElementById('cep')
     let cidade = document.getElementById('cidade')
     let estado = document.getElementById('estado')
-    const response = await fetch(`https://api.postmon.com.br/v1/cep/${postalCode}`);
-    const data = await response.json();
+    try {
+        const response = await fetch(`https://api.postmon.com.br/v1/cep/${postalCode}`);
+        if (!response.ok) {
+            throw new Error('CEP não encontrado. Por favor, verifique o CEP inserido.');
+        }
+        const data = await response.json();
         return data;
+    } catch (error) {
+        alert(error.message);
+        logradouro.value = ''
+        bairro.value = ''
+        cep.value = ''
+        cidade.value = ''
+        estado.value = ''
+        document.getElementById('usuario').value = ''
+    }
 }
-
     function procurar(){
         let user= document.getElementById('usuario').value
         if (!cepRegex.test(user.trim().replace(/-/g, ''))){
